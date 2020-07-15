74befe3e15409d6559b3ca10ea4f35faab589fa4
varenyamBakshi <varenyam101@gmail.com>
2020-07-15 20:20:20 +0530

    I think I have succesfully set up the postgresql but it is on localhost with a database on my machine.

--- wisaga/settings.py
+++ wisaga/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'wisaga.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wisagadb',
+        'USER': 'postgres',
+        'PASSWORD': 'wisagaforever',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
