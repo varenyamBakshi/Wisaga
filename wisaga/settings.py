2d5bce45609291c773b789960c06c92f2cfa5baf
varenyamBakshi <varenyam101@gmail.com>
2020-07-15 22:26:49 +0530

    established connection of the urls with variious views.

--- wisaga/settings.py
+++ wisaga/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'wisaga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'wisaga/templates')], #now it will look in the wisaga/templates folder for templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- wisaga/urls.py
+++ wisaga/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact-us/', views.contact_us, name="contact-us")
 ]
